9c459c9da65d9896f9029b413b3b39b3e8386cd1
djohnson135 <Dathanljohnson135@gmail.com>
2022-11-29 15:42:15 -0600

    fixing win32 issue

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -15,8 +15,26 @@ Plane::~Plane()
 
 float Plane::intersect(glm::vec3 origin, glm::vec3 ray, float t0, float t1)
 {
-	return FLT_MAX;
+	/*float t = FLT_MAX;
+
+	float epsilon = 0.00001f;
+	float pointVal = glm::dot(this->normal, ray);
+	if (abs(pointVal) > epsilon) {
+		t = glm::dot((this->center - origin), this->normal / pointVal);
+	}*/
+	//float denom = glm::dot(ray, this->normal);
+	
+	float t = (glm::dot((this->center - origin), this->normal)) / glm::dot(ray, this->normal);
+
+	if (t < t0 || t >t1) return FLT_MAX;
+	else return t;
+
+
+	//return FLT_MAX;
 }
 
-void Plane::getNormal() {}
+glm::vec3 Plane::getNormal()
+{
+	return this->normal;
+}
 
