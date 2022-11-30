00f0d976189a95c58dbd0a922aa6911cbab2fd9f
djohnson135 <Dathanljohnson135@gmail.com>
2022-11-30 14:38:10 -0600

    loading obj file

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 #include <iostream>
 #include "Camera.h"
 #include "Scene.h"
+#define TINYOBJLOADER_IMPLEMENTATION 
+#include "tiny_obj_loader.h"
+#include "Triangle.h"
 
 #define WINDOW_HEIGHT 800
 #define WINDOW_WIDTH 1200
@@ -18,7 +21,10 @@ std::vector<Light*> lights;
 
 bool isSceneBunny = false;
 	
+std::vector<glm::vec3> vertices;
+std::vector<glm::vec2> texCoords;
 
+std::vector<Triangle> triangleVector;
 
 
 void ClearFrameBuffer()
@@ -41,6 +47,66 @@ void CharacterCallback(GLFWwindow* lWindow, unsigned int key) {
 		break;
 	}
 }
+
+bool LoadModel(char* name, std::vector<glm::vec3>& vertices, std::vector<glm::vec2>& texCoords)
+{
+	// Taken from Shinjiro Sueda with slight modification
+	std::string meshName(name);
+	tinyobj::attrib_t attrib;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+	std::string errStr;
+	bool rc = tinyobj::LoadObj(&attrib, &shapes, &materials, &errStr, meshName.c_str());
+	if (!rc) {
+		std::cerr << errStr << std::endl;
+		return false;
+	}
+	else {
+		// Some OBJ files have different indices for vertex positions, normals,
+		// and texture coordinates. For example, a cube corner vertex may have
+		// three different normals. Here, we are going to duplicate all such
+		// vertices.
+		// Loop over shapes
+		for (size_t s = 0; s < shapes.size(); s++) {
+			// Loop over faces (polygons)
+			size_t index_offset = 0;
+			for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+				size_t fv = shapes[s].mesh.num_face_vertices[f];
+				// Loop over vertices in the face.
+				for (size_t v = 0; v < fv; v++) {
+					// access to vertex
+					tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+					vertices.push_back(glm::vec3(attrib.vertices[3 * idx.vertex_index + 0],
+						attrib.vertices[3 * idx.vertex_index + 1],
+						attrib.vertices[3 * idx.vertex_index + 2]));
+					if (!attrib.texcoords.empty()) {
+						texCoords.push_back(glm::vec2(attrib.texcoords[2 * idx.texcoord_index + 0],
+							attrib.texcoords[2 * idx.texcoord_index + 1]));
+					}
+				}
+				index_offset += fv;
+			}
+		}
+	}
+	return true;
+}
+
+//void CreateTriangleVector(std::vector<glm::vec3>& vertices, std::vector<glm::vec2>& texCoords)
+//{
+//	for (int i = 0; i < vertices.size() / 3; i++)
+//	{
+//		Triangle myTriangle;
+//
+//		if (texCoords.empty())
+//			myTriangle = Triangle(vertices[i * 3 + 0], vertices[i * 3 + 1], vertices[i * 3 + 2]);
+//		else
+//			myTriangle = Triangle(vertices[i * 3 + 0], vertices[i * 3 + 1], vertices[i * 3 + 2],
+//				texCoords[i * 3 + 0], texCoords[i * 3 + 1], texCoords[i * 3 + 2]);
+//
+//		triangleVector.push_back(myTriangle);
+//	}
+//}
+
 
 void Init()
 {
@@ -81,8 +147,9 @@ void Init()
 		scene = Scene();
 	}
 	else {
-		//LoadModel("../obj/bunny.obj");
+		LoadModel("../obj/bunny.obj", vertices, texCoords);
 		scene = Scene(true);
+		//CreateTriangleVector(vertices, texCoords);
 	}
 	
 	
@@ -97,6 +164,8 @@ void Init()
 	float *renderedImage = camera.GetRenderedImage();
 	memcpy(frameBuffer, renderedImage, sizeof(float) * WINDOW_HEIGHT * WINDOW_WIDTH * 3);
 }
+
+
 
 
 int main()
